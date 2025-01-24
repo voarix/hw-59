9773c5917f152d3081df4a1ca6a1a69d84ee4929
Kachkynov Alikhan <t8t228@gmail.com>
2025-01-24 16:45:05 +0600

    lvl 2) Делаю запрос по API и вывожу результат

--- src/containers/Joke/Joke.tsx
+++ src/containers/Joke/Joke.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+
 const Joke = () => {
-  return <div></div>;
+  const [joke, setJoke] = useState<string>("");
+
+  useEffect( () => {
+    const fetchData = async () => {
+      try{
+        const response = await fetch("https://api.chucknorris.io/jokes/random");
+
+        if(!response.ok){
+          throw new Error(response.status + ' - ' + response.statusText);
+        }
+        const newResponse = await response.json();
+        setJoke(newResponse.value);
+        console.log(newResponse);
+      } catch(err) {
+        alert(err);
+      }
+    };
+
+
+    void fetchData();
+  }, []);
+
+  return (
+    <div className='container'>
+      {joke ? <p>{joke}</p> : <p>шуток еще нет))</p>}
+    </div>
+  );
 };
 
 export default Joke;
